47b8c991a6690f5dd518496c52184b64d093ebda
NathanD0 <122122847+nathanbrz@users.noreply.github.com>
2025-01-20 14:08:52 -0700

    fixing deploy bug

--- frontend/src/sections/Contact.tsx
+++ frontend/src/sections/Contact.tsx
@@ -15,10 +15,10 @@ export const ContactSection = () => {
           <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
             <div>
               <h2 className="font-serif text-2xl md:text-3xl">
-                Let's create something amazing together!
+                Let&apos;s create something amazing together!
               </h2>
               <p className="text-sm md:text-base mt-2">
-                Ready to bring my livelyness to your team. Let's connect and
+                Ready to bring my livelyness to your team. Let&apos;s connect and
                 create something extraordinary.
               </p>
             </div>
